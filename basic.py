96afafd9c54cc0325ca9be2f05a355e197b32abf
yukonakauchi <48485745+yukonakauchi@users.noreply.github.com>
2019-03-13 06:34:55 +0100

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,29 @@
+"""
+Python does offer magic methods like sum() and others, please try to write down your algorithms or coding 
+instructions clearly without using any shortcuts
+"""
+# Multiply 3 and any number
+def mult(number):
+    # complete your code
+
+    
+# adds two numbers together
+def add(a, b):
+    # complete with your solution
+
+
+"""
+Using data structures like list, can you find the total or the values in that list.
+Do not use a function like sum(someList), that would be cheating ;-)
+"""
+# Data structure
+numbers = [1,2,3,6]
+def sumOfListNumbers(someList):
+    # your solution
+
+    
+
+# Program checker (do not change the lines below)    
+assert sumOfList(numbers) == 12
+assert mult(3) == 9
+assert add(1,3) == 4
